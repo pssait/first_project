a67f7eedf7b5dadaacd565edca04ff4b52e2c9a5
Sulaiman Sait <pssait@outlook.com>
2023-09-05 14:54:21 +0400

    Coding 1

---
+++ src/hackerrank/sixDays/one/Solution.java
@@ -0,0 +1,65 @@
+package hackerrank.sixDays.one;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void print(BigDecimal element) {
+        DecimalFormat formatter = new DecimalFormat("#0.000000");
+        System.out.println(formatter.format(element));
+    }
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+        AtomicInteger positive = new AtomicInteger();
+        AtomicInteger negative = new AtomicInteger();
+        AtomicInteger zero = new AtomicInteger();
+        AtomicInteger len = new AtomicInteger(arr.size());
+
+        arr.forEach(element -> {
+            if(element == 0) zero.getAndIncrement();
+            else if (element > 0) positive.getAndIncrement();
+            else negative.getAndIncrement();
+        });
+
+        float lenFloat = len.floatValue();
+
+        BigDecimal posProb = BigDecimal.valueOf(positive.floatValue() / lenFloat);
+        print(posProb);
+
+        BigDecimal negProb = BigDecimal.valueOf(negative.floatValue() / lenFloat);
+        print(negProb);
+
+        BigDecimal zeroProb = BigDecimal.valueOf(zero.floatValue() / lenFloat);
+        print(zeroProb);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
---
+++ src/other/Test.java
@@ -0,0 +1,15 @@
+package other;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Test {
+          // Java 8 - "java"
+     public static void main(String[] args) {
+          String testString = "java";
+          char[] testChar = testString.toCharArray();
+          //Stream<Character> charStream = Arrays.asList(testString.).stream();
+
+     }
+
+}
