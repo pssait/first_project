d1c97a88e7d11da46a0fc705a77dca7e81ea3255
Sulaiman Sait <pssait@outlook.com>
2023-09-19 16:21:28 +0400

    Coding 7

--- src/interview/optimum/question1/Solution.java
+++ src/interview/optimum/question1/Solution.java
@@ -2,6 +2,19 @@ package interview.optimum.question1;
 
 public class Solution {
     public static void main(String[] args) {
+//input : ZXCV
+//output VCXZ
+        StringBuilder sample = new StringBuilder("ZXCV");
+        sample.reverse();
+        System.out.println(sample);
 
+        String sample2 = "ZXCV";
+        int len = sample2.length();
+        char[] charSample = new char[len];
+        for(int i = len-1 ; i >= 0; i-- ) {
+            charSample[i] = sample2.charAt(len-i-1);
+        }
+        String output = new String(charSample);
+        System.out.println(output);
     }
 }
